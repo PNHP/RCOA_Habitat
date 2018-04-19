9b6f0cda6b999e53790efa5d6df88234d8cedd6e
Christopher Tracey <ChristopherTracey@users.noreply.github.com>
2018-04-19 11:39:48 -0400

    Update HabitatMaster.R

--- HabitatMaster.R
+++ HabitatMaster.R
@@ -1,1 +1,22 @@
 
+library(data.table)
+library(plyr)
+library(stringr)
+library(tidyr)
+
+setwd("E:/Dropbox (PNHP @ WPC)/RCOAassoc/201804/RCOA_Habitat")
+
+# fucntion for reading in habitat data
+myfunction <- function(arg1, arg2, ... ){
+  #habitat <- read.csv("Habitat_DSLDistance.csv", na.strings=c("NA")) # DSL and Distance to Water
+  #habitat$habitatcode <- paste(habitat$BaseHabitatData1,habitat$BaseHabitatData2, sep="_")
+  statements
+return(object)
+}
+
+
+# function to created summarized habitat weights
+myfunction <- function(arg1, arg2, ... ){
+statements
+return(object)
+}
